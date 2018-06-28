3cc2c0d45824f1f772987b84e6a5fbf27bde1be4
Administrator <administrator@gbtc408016dt.montgomerycollege.edu>
2018-06-28 14:04:39 -0400

    update

--- Segues/ViewController.swift
+++ Segues/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var displayUser: UIBarButtonItem!
+    @IBOutlet weak var btnLogIn: UIButton!
+    @IBOutlet weak var btnForgotUN: UIButton!
+    @IBOutlet weak var btnForgotPW: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +28,37 @@ class ViewController: UIViewController {
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = username.text
+        
+        guard let sender = sender as? UIButton else {return}
+        
+        if sender == btnForgotPW {
+            segue.destination.navigationItem.title = "Forgot Password"
+        }
+        else if sender == btnForgotUN {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = username.text
+        }
+        
     }
     
+
     /*
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         segue.destination.navigationItem.title = textField.text
     }
 */
-
+    @IBAction func goToConfirmLogin(_ sender: Any) {
+        performSegue(withIdentifier: "User", sender: nil)
+    }
+    
+    @IBAction func findUserName(_ sender: Any) {
+        performSegue(withIdentifier: "Forgot Username", sender: nil)
+    }
+    
+    @IBAction func findPassword(_ sender: Any) {
+        performSegue(withIdentifier: "Forgot Password", sender: nil)    }
+    
 }
 
 
